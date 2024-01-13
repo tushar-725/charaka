b80a3c016448d75c039b94a20fa70887531e630e
tushar-725 <tushar7.2510@gmail.com>
2024-01-14 02:13:29 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//components
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Blog from './components/blog/Blog';
+import Vivaad from './components/programs/vivaad/Vivaad';
+import Samvaad from './components/programs/samvaad/Samvaad';
+import Compendium from './components/programs/compendium/Compendium';
+import Research from './components/programs/compendium-sub/Research';
+import Narration from './components/programs/compendium-sub/Narration';
+import Team from './components/team/team';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header />
+      <div className="App">
+        <Routes>  
+          <Route path='/' element={ <Home /> } />
+          <Route path='/blog' element={ <Blog /> } />
+          <Route path='/vivaad' element={ <Vivaad /> } />
+          <Route path='/samvaad' element={ <Samvaad />} />
+          <Route path='/compendium' element={ <Compendium />} />
+          <Route path='/research-101' element={ <Research /> } />
+          <Route path='/monthly-narration' element={ <Narration /> } />
+          <Route path='/our-team' element={ <Team /> } />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
